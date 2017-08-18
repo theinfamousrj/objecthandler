8c8f22fa05ebbcac281797a53acef51e42cdbb63
Raymond John Hill <rhill@mapanything.com>
2017-08-18 19:26:41 +0000

    Added the index.js, the meat of the npm.

---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  getKeys: function(obj, root, keys) {
+    if(typeof obj !== 'object') throw new Error('Item to check must be an object.')
+    
+    for(let prop in obj) {
+        let curr = root !== null ? root+'.'+prop : prop
+        if(typeof obj[prop] === 'object') {
+            this.getKeys(obj[prop], curr, keys)
+        }
+        keys.push(curr)
+    }
+  },
+  hasRequiredKeys: function(obj, keys) {
+    if(typeof obj !== 'object') throw new Error('Item to check must be an object.')
+    
+    let objKeys = []
+    this.getKeys(obj, null, objKeys)
+    
+    let hasKeys = true
+    for(let i=0, l=keys.length; i<l; i++) {
+      let key = keys[i]
+      hasKeys = hasKeys && objKeys.indexOf(key) !== -1
+    }
+    
+    return hasKeys;
+  }
+};
